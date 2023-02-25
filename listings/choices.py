d174f0f8b5e0efc9285a4cd4291ee62217c5e97f
testpageAN <alexis.nikolopoulos@hotmail.com>
2023-02-25 11:29:10 +0200

    25-02-2023 comment out tables.py and def table-test 4

--- listings/choices.py
+++ listings/choices.py
@@ -13,10 +13,11 @@ except Listing.DoesNotExist:
 
 units_choices = {}
 sorted_units_keys = []
-for listing in listings:
-    if listing.unit not in sorted_units_keys:
-        sorted_units_keys.append(listing.unit)
-sorted_units_keys.sort()
+if listings:
+    for listing in listings:
+        if listing.unit not in sorted_units_keys:
+            sorted_units_keys.append(listing.unit)
+    sorted_units_keys.sort()
 
 for listing in sorted_units_keys:
     if listing not in units_choices.keys():
@@ -27,10 +28,11 @@ for listing in sorted_units_keys:
 
 intervals_choices = {}
 sorted_intervals_keys = []
-for listing in listings:
-    if listing.interval not in sorted_intervals_keys:
-        sorted_intervals_keys.append(listing.interval)
-sorted_intervals_keys.sort()
+if listings:
+    for listing in listings:
+        if listing.interval not in sorted_intervals_keys:
+            sorted_intervals_keys.append(listing.interval)
+    sorted_intervals_keys.sort()
 
 for listing in sorted_intervals_keys:
     if listing not in intervals_choices.keys():
@@ -41,10 +43,11 @@ for listing in sorted_intervals_keys:
 
 measure_units_choices = {}
 sorted_measure_units_keys = []
-for listing in listings:
-    if listing.units not in sorted_measure_units_keys:
-        sorted_measure_units_keys.append(listing.units)
-sorted_measure_units_keys.sort()
+if listings:
+    for listing in listings:
+        if listing.units not in sorted_measure_units_keys:
+            sorted_measure_units_keys.append(listing.units)
+    sorted_measure_units_keys.sort()
 
 for listing in sorted_measure_units_keys:
     if listing not in measure_units_choices.keys():
@@ -55,9 +58,10 @@ for listing in sorted_measure_units_keys:
 
 realtors_choices = {}
 sorted_realtors_keys = []
-for listing in listings:
-    if listing.realtor not in sorted_realtors_keys:
-        sorted_realtors_keys.append(listing.realtor)
+if listings:
+    for listing in listings:
+        if listing.realtor not in sorted_realtors_keys:
+            sorted_realtors_keys.append(listing.realtor)
 
 for listing in sorted_realtors_keys:
     if listing not in realtors_choices.keys():
@@ -68,9 +72,10 @@ for listing in sorted_realtors_keys:
 
 blocks_choices = {}
 sorted_blocks_keys = []
-for listing in listings:
-    if listing.block not in sorted_blocks_keys:
-        sorted_blocks_keys.append(listing.block)
+if listings:
+    for listing in listings:
+        if listing.block not in sorted_blocks_keys:
+            sorted_blocks_keys.append(listing.block)
 
 for listing in sorted_blocks_keys:
     if listing not in blocks_choices.keys():
@@ -81,9 +86,10 @@ for listing in sorted_blocks_keys:
 
 types_choices = {}
 sorted_types_keys = []
-for listing in listings:
-    if listing.type not in sorted_types_keys:
-        sorted_types_keys.append(listing.type)
+if listings:
+    for listing in listings:
+        if listing.type not in sorted_types_keys:
+            sorted_types_keys.append(listing.type)
 
 for listing in sorted_types_keys:
     if listing not in types_choices.keys():
@@ -94,9 +100,10 @@ for listing in sorted_types_keys:
 
 special_types_choices = {}
 sorted_special_types_keys = []
-for listing in listings:
-    if listing.special_type not in sorted_special_types_keys:
-        sorted_special_types_keys.append(listing.special_type)
+if listings:
+    for listing in listings:
+        if listing.special_type not in sorted_special_types_keys:
+            sorted_special_types_keys.append(listing.special_type)
 
 for listing in sorted_special_types_keys:
     if listing not in special_types_choices.keys():
